ec64de228e8a56115c4e2562d8270c33613fa819
Ernest Guevarra <ernest@guevarra.io>
2026-01-22 15:14:13 +0000

    update questionnaire chapter; ixes #26

--- packages.R
+++ packages.R
@@ -1,3 +1,20 @@
 # Install R package dependencies -----------------------------------------------
 
+
+## General tools for R workflows ----
+
+library(dplyr)
+library(ggplot2)
+
+
+## General packages for quarto publications ----
+
 library(quarto)
+
+
+## Packages for working with PDFs ----
+
+library(pdftools)
+library(tesseract)
+
+
